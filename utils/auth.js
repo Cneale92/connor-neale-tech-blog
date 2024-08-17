bb3c68c5ebeb2e94d3c42da1aade2ddd7b707e6f
Cneale92 <connorneale92@gmail.com>
2024-08-17 13:58:18 -0500

    added authentication code

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+// Authentication middleware utility to force the user to go to the login screen if not logged in
+
+const withAuth = (req, res, next) => {
+  if (!req.session.logged_in) {
+    res.redirect("/login");
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
